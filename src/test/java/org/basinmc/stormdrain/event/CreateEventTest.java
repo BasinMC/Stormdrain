61a67e09695d1859ce9292cf81554dc8597033d8
Johannes Donath <johannesd@torchmind.com>
2018-03-08 01:58:36 +0100

    Created some basic tests for all existing event types.

---
+++ src/test/java/org/basinmc/stormdrain/event/CreateEventTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.stormdrain.event;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import org.basinmc.stormdrain.event.AbstractReferenceEvent.ReferenceType;
+import org.junit.Assert;
+
+/**
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class CreateEventTest extends AbstractEventParserTest<CreateEvent> {
+
+  public CreateEventTest() {
+    super("create", CreateEvent.class);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void doTest(@NonNull CreateEvent model) {
+    Assert.assertEquals("0.0.1", model.getReference());
+    Assert.assertEquals(ReferenceType.TAG, model.getReferenceType());
+    Assert.assertEquals("master", model.getDefaultBranch());
+    Assert.assertNotNull(model.getRepository());
+    Assert.assertNotNull(model.getSender());
+  }
+}
---
+++ src/test/java/org/basinmc/stormdrain/event/MemberEventTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2018 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.stormdrain.event;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import org.basinmc.stormdrain.event.MemberEvent.Action;
+import org.junit.Assert;
+
+/**
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class MemberEventTest extends AbstractEventParserTest<MemberEvent> {
+
+  public MemberEventTest() {
+    super("member", MemberEvent.class);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void doTest(@NonNull MemberEvent model) {
+    Assert.assertEquals(Action.ADDED, model.getAction());
+    Assert.assertNotNull(model.getMember());
+    Assert.assertNotNull(model.getRepository());
+    Assert.assertNotNull(model.getSender());
+  }
+}
